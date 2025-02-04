0577d3c5151d016d63a6f78af0af87e0f12f4541
JacksonBell09 <jacksonbellschool@gmail.com>
2025-02-04 13:11:48 -0500

    added scripts

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// jackson Bell
+public class Movement : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public float jumpforce = 20f;
+    [SerializeField] private bool IsJumping = false;
+
+    private Rigidbody2D rb;
+    //start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
+
+//update is called once per frame
+    void Update()
+    {
+    //code for horizontal movement
+    float horizontalInput = Input.GetAxis("Horizontal");
+    Vector2 moveVector = new Vector2 (horizontalInput * movespeed, rb.velocity.y);
+    //player is jumping
+    if (Input.GetButtonDown("Jump") && !IsJumping){
+        moveVector.y = jumpforce;
+        IsJumping = true;
+    }
+    rb.velocity = moveVector;
+
+    if (horizontalInput < 0){
+        transform.localScale = new Vector3(1, 1, 1);
+    } else if (horizontalInput > 0){
+        transform.localScale = new Vector3(-1, 1, 1);
+    }
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collison){
+    if(collison.gameObject.CompareTag("Ground")){
+        IsJumping = false;
+    }
+    }
+}
